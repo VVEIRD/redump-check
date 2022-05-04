d1100c44b8e936da39e00e3a838cab712b9562dc
VVEIRD <vveird.oss@gmail.com>
2022-05-04 04:09:46 +0200

    Autoinstall off appdirs added

--- redump.bat
+++ redump.bat
@@ -1,3 +1,8 @@
 @echo off
+pip list | findstr appdirs > nul
+if %errorlevel% == 1 (
+    echo Module appdirs is missing, installing
+    pip install appdirs
+)
 python %~dp0\%~n0.py %*
 @echo on
--- redump.sh
+++ redump.sh
@@ -2,4 +2,9 @@
 filename=$(basename -- "$0")
 filename="${filename%.*}"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+c=$(pip list | grep appdirs | wc -l)
+if [ ${c} -neq 0 ]; then
+    echo Module appdirs is missing, installing
+    pip install appdirs
+fi
 python ${SCRIPT_DIR}/${filename}.py $*
